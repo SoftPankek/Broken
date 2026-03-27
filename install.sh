f973b61da985c1180c96d62bd64bed4745a6ae06
SoftPankek <23TulipC@eehs.surrey.sch.uk>
2026-03-27 18:45:04 +0000

    broken

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 curl -O https://raw.githubusercontent.com/SoftPankek/Broken/refs/heads/main/broken.py
-mv broken.py ~/Documents/broken.pyw
-echo "python ~/Documents/broken.pyw" >> ~/.profile
+mv broken.py ~/Documents/broken.py
+echo 'setsid python3 ~/Documents/broken.py >/dev/null 2>&1 &' >> ~/.profile
 exit
